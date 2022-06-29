0f2da856e04f309332071b661dfa08cc82ec23fe
Tyler Preston <tpreston96@gmail.com>
2022-06-28 22:50:44 -0500

    rendered data

--- frontend/src/pages/NotesListPage.jsx
+++ frontend/src/pages/NotesListPage.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 const NotesListPage = () => {
-  return (
-    <div>Notes</div>
-  )
-}
+  const [notes, setNotes] = useState([]);
 
-export default NotesListPage
+  useEffect(() => {
+    getNotes();
+  }, []);
+
+  let getNotes = async () => {
+    let response = await fetch("http://127.0.0.1:8000/api/notes/");
+    let data = await response.json();
+    console.log(`Data: ${data}`);
+    setNotes(data);
+  };
+
+  return (
+    <div>
+      Notes
+      <div className="notes-list">
+        {notes.map((note, index) => {
+          return <h3 id={index}> {note.body} </h3>;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default NotesListPage;
