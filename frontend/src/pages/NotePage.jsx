463c01cc14ae1f3c8200fbe11367676447625f2e
Tyler Preston <tpreston96@gmail.com>
2022-06-28 23:47:54 -0500

    useParams

--- frontend/src/pages/NotePage.jsx
+++ frontend/src/pages/NotePage.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
 
 const NotePage = () => {
-  return (
-    <h1>Single Note</h1>
-  )
-}
+  let params = useParams();
+  return <h1>Single Note {params.id}</h1>;
+};
 
-export default NotePage
+export default NotePage;
