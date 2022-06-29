793b3bec6cfce635fb7b59488fe29553ba7b1df2
Tyler Preston <tpreston96@gmail.com>
2022-06-28 19:17:43 -0500

    url for get_note

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_routes, name='routes'),
-    path("notes/", views.get_notes, name="get_notes")
+    path("", views.get_routes, name="routes"),
+    path("notes/", views.get_notes, name="notes"),
+    path("notes/<int:pk>/", views.get_note, name="note"),
 ]
