e622f5acfcc990ab70586de840da466ceb9147da
Tyler Preston <tpreston96@gmail.com>
2022-06-29 00:24:26 -0500

    Create NotePage.jsx

--- frontend/src/pages/NotePage.jsx
+++ frontend/src/pages/NotePage.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 const NotePage = () => {
+  const [note, setNote] = useState(null);
   let params = useParams();
-  return <h1>Single Note {params.id}</h1>;
+
+  useEffect(() => {
+    getNote();
+  }, [params.id]);
+
+  let getNote = async () => {
+    let response = fetch(`/api/notes/${params.id}/`);
+    let data = await (await response).json();
+    setNote(data);
+  };
+
+  return (
+    <h1>
+      <p>
+        {note?.body} {params.id}
+      </p>
+    </h1>
+  );
 };
 
 export default NotePage;
