a635ec8eee1971b539c8d5c8a242ea7ed1dccf62
Tyler Preston <tpreston96@gmail.com>
2022-06-29 16:20:03 -0500

    Add notes-header to NotesListPage.jsx

--- frontend/src/pages/NotesListPage.jsx
+++ frontend/src/pages/NotesListPage.jsx
@@ -15,11 +15,15 @@ const NotesListPage = () => {
   };
 
   return (
-    <div>
-      Notes
+    <div className="notes">
+      <div className="notes-header">
+        <h2 className="notes-title">&#9782; Notes</h2>
+        <p className="notes-count">{notes.length}</p>
+      </div>
+
       <div className="notes-list">
         {notes.map((note, index) => {
-          return <ListItem note={note} key={index} />;
+          return <ListItem key={index} note={note} />;
         })}
       </div>
     </div>
