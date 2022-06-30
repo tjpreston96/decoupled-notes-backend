b7de8f844a4155023141956bad9317142122f224
Tyler Preston <tpreston96@gmail.com>
2022-06-30 18:17:41 -0500

    update paths for restful api

--- api/urls.py
+++ api/urls.py
@@ -3,14 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.get_routes, name="routes"),
-    # Index
     path("notes/", views.get_notes, name="notes"),
-    # Create
-    path("notes/create/", views.create_note, name="create_note"),
-    # Update
-    path("notes/<int:pk>/update/", views.update_note, name="update_note"),
-    # Delete
-    path("notes/<int:pk>/delete/", views.delete_note, name="delete_note"),
-    # Render
-    path("notes/<int:pk>/", views.get_note, name="get_note"),
+    path("notes/<int:pk>/", views.get_note, name="note"),
 ]
