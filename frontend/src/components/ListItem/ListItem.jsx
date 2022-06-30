c13dde356d9c4cb8cc80b6026c809bd5cc1993f6
Tyler Preston <tpreston96@gmail.com>
2022-06-30 00:32:36 -0500

    getTitle && getTime

--- frontend/src/components/ListItem/ListItem.jsx
+++ frontend/src/components/ListItem/ListItem.jsx
@@ -1,12 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+let getTime = (note) => {
+  return new Date(note.updated).toLocaleDateString();
+};
+
+let getTitle = (note) => {
+  // split at first linebreak
+  let title = note.body.split("\n")[0];
+
+  if (title.length > 45) {
+    return title.slice(0, 45);
+  }
+  return title;
+};
+
 const ListItem = ({ note }) => {
   return (
     <div>
       <Link to={`/note/${note.id}`}>
         <div className="notes-list-item">
-          <h3>{note.body}</h3>
+          <h3>{getTitle(note)}</h3>
+          <p>
+            <span>{getTime(note)}</span>
+          </p>
         </div>
       </Link>
     </div>
