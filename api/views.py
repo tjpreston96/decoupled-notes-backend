4b0a66a56aae6b3b6795e581dcb417bb7ea6e644
Tyler Preston <tpreston96@gmail.com>
2022-06-30 00:00:49 -0500

    view for create_note

--- api/views.py
+++ api/views.py
@@ -60,6 +60,14 @@ def get_note(request, pk):
     return Response(serializer.data)
 
 
+@api_view(["POST"])
+def create_note(request):
+    data = request.data
+    note = Note.objects.create(body=data["body"])
+    serializer = NoteSerializer(note, many=False)
+    return Response(serializer.data)
+
+
 @api_view(["GET", "PUT"])
 def update_note(request, pk):
     data = request.data
