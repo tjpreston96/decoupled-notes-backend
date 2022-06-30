54ccca5155cdcdd7f1346a9fe09a33c72608b14f
Tyler Preston <tpreston96@gmail.com>
2022-06-30 18:15:21 -0500

    create utility functions from views

---
+++ api/utils.py
@@ -0,0 +1,77 @@
+from rest_framework.response import Response
+
+from .models import Note
+from .serializers import NoteSerializer
+
+
+def routes_list(request):
+    routes = [
+        {
+            "Endpoint": "/notes/",
+            "method": "GET",
+            "body": None,
+            "description": "Returns an array of notes",
+        },
+        {
+            "Endpoint": "/notes/id",
+            "method": "GET",
+            "body": None,
+            "description": "Returns a single note object",
+        },
+        {
+            "Endpoint": "/notes/create/",
+            "method": "POST",
+            "body": {"body": ""},
+            "description": "Creates new note with data sent in post request",
+        },
+        {
+            "Endpoint": "/notes/id/update/",
+            "method": "PUT",
+            "body": {"body": ""},
+            "description": "Creates an existing note with data sent in post request",
+        },
+        {
+            "Endpoint": "/notes/id/delete/",
+            "method": "DELETE",
+            "body": None,
+            "description": "Deletes and exiting note",
+        },
+    ]
+    
+    return Response(routes)
+
+
+def notes_list(request):
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
+
+
+def note_detail(request, pk):
+    notes = Note.objects.get(id=pk)
+    serializer = NoteSerializer(notes, many=False)
+    return Response(serializer.data)
+
+
+def create_note(request):
+    data = request.data
+    note = Note.objects.create(body=data["body"])
+    serializer = NoteSerializer(note, many=False)
+    return Response(serializer.data)
+
+
+def update_note(request, pk):
+    data = request.data
+    note = Note.objects.get(id=pk)
+    serializer = NoteSerializer(instance=note, data=data, many=False)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+def delete_note(request, pk):
+    note = Note.objects.get(id=pk)
+    note.delete()
+    return Response("Note was deleted!")
