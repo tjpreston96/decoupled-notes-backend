beb6846533510a3a2ae22ac626bb33518ed7d375
Tyler Preston <tpreston96@gmail.com>
2022-06-30 01:00:50 -0500

    create getContent

--- frontend/src/components/ListItem/ListItem.jsx
+++ frontend/src/components/ListItem/ListItem.jsx
@@ -15,6 +15,18 @@ let getTitle = (note) => {
   return title;
 };
 
+let getContent = (note) => {
+  let title = getTitle(note);
+  let content = note.body.replaceAll("\n", " ");
+  content = content.replaceAll(title, "");
+
+  if (content.length > 45) {
+    return content.slice(0, 45) + "...";
+  } else {
+    return content;
+  }
+};
+
 const ListItem = ({ note }) => {
   return (
     <div>
@@ -22,7 +34,7 @@ const ListItem = ({ note }) => {
         <div className="notes-list-item">
           <h3>{getTitle(note)}</h3>
           <p>
-            <span>{getTime(note)}</span>
+            <span>{getTime(note)}</span>{getContent(note)}
           </p>
         </div>
       </Link>
