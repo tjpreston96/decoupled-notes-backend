d887ed3ee3a2c4d8624b729bdb59dd3b3b8f57be
Tyler Preston <tpreston96@gmail.com>
2022-06-30 20:30:03 -0500

    heroku

--- mynotes/settings.py
+++ mynotes/settings.py
@@ -129,12 +129,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-django_heroku.settings(locals())
 
 STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [BASE_DIR / "frontend/build/static"]
 
+django_heroku.settings(locals())
 # For React.js Development
 # CORS_ALLOWED_ORIGINS = [
 #     "http://localhost:3000",
